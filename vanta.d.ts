8dfceca29e19d90e60a489abbfaac51f844ee144
Tushar Pachouri <tusharpachouri001@gmail.com>
2026-06-23 19:51:41 +0530

    feat: add TypeScript declaration file for Vanta.js modules

---
+++ vanta.d.ts
@@ -0,0 +1,64 @@
+declare module 'vanta/dist/vanta.birds.min' {
+  const birds: any;
+  export default birds;
+}
+
+declare module 'vanta/dist/vanta.fog.min' {
+  const fog: any;
+  export default fog;
+}
+
+declare module 'vanta/dist/vanta.waves.min' {
+  const waves: any;
+  export default waves;
+}
+
+declare module 'vanta/dist/vanta.clouds.min' {
+  const clouds: any;
+  export default clouds;
+}
+
+declare module 'vanta/dist/vanta.clouds2.min' {
+  const clouds2: any;
+  export default clouds2;
+}
+
+declare module 'vanta/dist/vanta.globe.min' {
+  const globe: any;
+  export default globe;
+}
+
+declare module 'vanta/dist/vanta.net.min' {
+  const net: any;
+  export default net;
+}
+
+declare module 'vanta/dist/vanta.cells.min' {
+  const cells: any;
+  export default cells;
+}
+
+declare module 'vanta/dist/vanta.topology.min' {
+  const topology: any;
+  export default topology;
+}
+
+declare module 'vanta/dist/vanta.dots.min' {
+  const dots: any;
+  export default dots;
+}
+
+declare module 'vanta/dist/vanta.rings.min' {
+  const rings: any;
+  export default rings;
+}
+
+declare module 'vanta/dist/vanta.halo.min' {
+  const halo: any;
+  export default halo;
+}
+
+declare module 'vanta/dist/vanta.trunk.min' {
+  const trunk: any;
+  export default trunk;
+}
